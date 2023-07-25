fd11605cff28dbda1ef065096abea34ddbc009e8
Inouedev1 <nikollasinoue.contato@gmail.com>
2023-07-25 17:21:27 -0300

    const to var and css
    Trocar var por const e css coisas

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 function exibirMensagemValidacao(mensagem, valido) {
-  var mensagemElemento = document.getElementById("mensagem-validacao");
+  const mensagemElemento = document.getElementById("mensagem-validacao");
 
   if (valido) {
-    mensagemElemento.innerHTML = mensagem + '<span class="dismiss-button" onclick="fecharMensagem()">Dismiss</span>';
+    mensagemElemento.innerHTML = mensagem + '<span class="dismiss-button" onclick="fecharMensagem()">Dismiss message</span>';
     mensagemElemento.className = "mensagem-validacao valido";
     mensagemElemento.style.display = "block";
     everything.style.display = "none";
     resultado.style.display = "none";
+    img1.style.display = "none";
+    img2.style.display = "block";
   } else {
     mensagemElemento.style.display = "none";
   }
 }
 
 function fecharMensagem() {
-  var mensagemElemento = document.getElementById("mensagem-validacao");
+  const mensagemElemento = document.getElementById("mensagem-validacao");
   mensagemElemento.style.display = "none";
   everything.style.display = "block";
+  img1.style.display = "block";
+  img2.style.display = "none";
 }
 
 function validarEmail() {
-  var email = document.getElementById("email").value;
-  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = document.getElementById("email").value;
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (regex.test(email)) {
-    var mensagem = '<h1>Thanks for subscribing</h1>' + '<p> A confirmation email has been sent to ' + email + '. Please open it and click the button inside to confirm your subscription.</p>';
+    const mensagem = '<h1>Thanks for subscribing</h1>' + '<p> A confirmation email has been sent to <strong>' + email + 
+    '</strong>. Please open it and click the button inside to confirm your subscription.</p>';
     exibirMensagemValidacao(mensagem, true);
   } else {
     resultado.innerHTML = "Valid email required.";
     resultado.style.color = "red";
+    resultado.style.display = "block";
+
   }
 }
